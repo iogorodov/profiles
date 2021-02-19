3bf49d61092e01cdbe0816aa474e0d6179f539be
iogorodov <iogorodov@gmail.com>
2021-02-19 11:23:10 +0300

    Create windows_new_pc.cmd

---
+++ windows_new_pc.cmd
@@ -0,0 +1,35 @@
+winget install Google.Chrome
+winget install Mozilla.Firefox
+winget install VideoLAN.VLC
+winget install XnSoft.XnViewClassic
+
+winget install Telegram.TelegramDesktop
+winget install Microsoft.Skype
+
+winget install Microsoft.WindowsTerminal
+winget install Git.Git -i
+winget install 7zip.7zip
+
+winget install SublimeHQ.SublimeText
+winget install SublimeHQ.SublimeMerge
+
+winget install Oracle.MySQL --version 5.7.31 -i
+winget install OpenJS.Nodejs --version 12.18.3 -i
+winget install AdoptOpenJDK.OpenJDK --version 8.0.265 -i
+
+winget install SourceFoundry.HackFonts
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+winget install Canonical.Ubuntu
+
+rem Intellij IDEA
+rem TablePlus
+rem Cloudshot
+rem Curl
+rem Atlassian SDK
+rem Paint.NET
+rem qBittorrent 64
+rem Total Commander
+rem Outline
+rem Spotify
+
